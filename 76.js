525482a9ea9ad7a183a2da5f6578cd103dcc0b13
nawedzan <nawedzan2016@gmail.com>
2024-06-03 23:21:22 +0530

    76.js

---
+++ 76.js
@@ -0,0 +1,26 @@
+// a shorter syntax
+
+// function in html is called method
+//  a shorter syntax can be used by calling the key as function directly
+
+// const user1 = {
+//     firstName : "harshit",
+//     age: 8,
+//     about: function(){
+//         console.log(this.firstName, this.age);
+//     }   
+// }
+
+// const user1 = {
+//     firstName : "harshit",
+//     age: 8,
+//     about(){
+//         console.log(this.firstName, this.age);
+//     }   
+// }
+
+
+
+user1.about();
+
+

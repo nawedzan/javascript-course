bae490960bb43a97faea9fce7fc0349f03efa8eb
nawedzan <nawedzan2016@gmail.com>
2024-06-03 23:17:34 +0530

    75.js

---
+++ 75.js
@@ -0,0 +1,14 @@
+// arrow functions 
+
+const user1 = {
+    firstName : "harshit",
+    age: 8,
+    about: () => {
+        console.log(this.firstName, this.age);
+    }   
+}
+
+user1.about(user1);
+
+// arrow funciton takes the this of its lexical environment 
+// in the case of above example the this keyword points to window object

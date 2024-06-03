3a02e4103fdf2532712278cd52f00d4999c896dc
nawedzan <nawedzan2016@gmail.com>
2024-06-03 20:52:07 +0530

    70.js

---
+++ 70.js
@@ -0,0 +1,24 @@
+// -------------OPTIONAL CHAINING----------------------------
+// in optional chaining we chain the properties of properties with a '?' 
+// now if say a there is a object of object like user.address.home
+//but home is not availbale so the js compiler can put an error there to avoid this
+// we use '?' user?.addresss?.home 
+// the '?' mark ensueres if hte user is presesnt thant search for its address
+// if not than return undefined
+const user={
+    Name:"nawed",
+    age:18,
+    about:function(){
+        console.log(user.Name,user.age)
+        return null
+    },
+    // address:{home:"sundarapada",office:"california",
+    // }
+}
+// console.log(user)
+// console.log('\n')
+// console.log(user.about())
+// console.log('\n')
+// user.about()
+console.log(user?.address?.home)
+// ---------------------------------------------
---
+++ readme.md
@@ -0,0 +1,2 @@
+this repo is for practice and revision of js course
+harsh vashist js
